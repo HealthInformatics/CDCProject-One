f61bde90b8e41601780608304589b9e6d70e5e14
wawjchinawct <wawjchinawct@gmail.com>
2016-04-10 15:57:56 -0400

    firebase.

--- demo/src/main/java/edu/gatech/cdcproject/demo/settings/SettingsActivity.java
+++ demo/src/main/java/edu/gatech/cdcproject/demo/settings/SettingsActivity.java
@@ -4,6 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 import edu.gatech.cdcproject.demo.R;
 
@@ -11,6 +17,8 @@ import edu.gatech.cdcproject.demo.R;
  * Created by guoweidong on 3/28/16.
  */
 public class SettingsActivity extends AppCompatActivity {
+    private Button logInButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +37,25 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View v) {
                 SettingsActivity.this.onBackPressed();
             }
+        });
+        logInButton = (Button) findViewById(R.id.loginBt);
+    }
+
+    public void myLogin(View v){
+        Firebase myFirebaseRef = new Firebase("https://glaring-fire-1928.firebaseio.com/");
+        myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
+
+        myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+            }
+
+            @Override
+            public void onCancelled(FirebaseError error) {
+            }
+
         });
     }
 
