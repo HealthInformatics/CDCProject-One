b2bd6186a1121ea5b18e8b6ee58617d724fc3129
wawjchinawct <wawjchinawct@gmail.com>
2016-04-28 12:35:16 -0400

    delete hapi1.1

--- demo/src/main/java/edu/gatech/cdcproject/demo/healthrecord/HealthRecordFragment.java
+++ demo/src/main/java/edu/gatech/cdcproject/demo/healthrecord/HealthRecordFragment.java
@@ -77,6 +77,28 @@ public class HealthRecordFragment extends Fragment {
                     System.out.println(ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(responseBundle));
                     */
 
+                    /*Patient patient = new Patient();
+                    // ..populate the patient object..
+                    patient.addIdentifier().setSystem("urn:system").setValue("12345");
+                    patient.addName().addFamily("Smith").addGiven("John");
+
+                    // Invoke the server create method (and send pretty-printed JSON
+                    // encoding to the server
+                    // instead of the default which is non-pretty printed XML)
+                    MethodOutcome outcome = client.create()
+                            .resource(patient)
+                            .prettyPrint()
+                            .encodedJson()
+                            .execute();
+
+                    // The MethodOutcome object will contain information about the
+                    // response from the server, including the ID of the created
+                    // resource, the OperationOutcome response, etc. (assuming that
+                    // any of these things were provided by the server! They may not
+                    // always be)
+                    IdDt id = (IdDt) outcome.getId();
+                    System.out.println("Got ID: " + id.getValue());*/
+
 
                 String dataURL="http://polaris.i3l.gatech.edu:8080/gt-fhir-webapp/base/Patient/" + SettingsActivity.ID +"?_format=json";
 
