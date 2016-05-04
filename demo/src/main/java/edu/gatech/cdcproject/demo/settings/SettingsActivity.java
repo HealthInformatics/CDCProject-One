0a523e44b23de0857c5d59efbd2fead73d48a082
dingfs <dingfengshao@gmail.com>
2016-05-03 20:22:19 -0400

    change setting ui

--- demo/src/main/java/edu/gatech/cdcproject/demo/settings/SettingsActivity.java
+++ demo/src/main/java/edu/gatech/cdcproject/demo/settings/SettingsActivity.java
@@ -1,5 +1,6 @@
 package edu.gatech.cdcproject.demo.settings;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +15,15 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import edu.gatech.cdcproject.demo.R;
 
 
@@ -25,7 +35,7 @@ public class SettingsActivity extends AppCompatActivity {
     public static String ID;
     private EditText editText_0;
     private EditText editText_1;
-
+    String personal_info;
     private TextView healthInfo;
     private Button logOutBtn;
 
@@ -57,6 +67,9 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         if(ID == null){
             setContentView(R.layout.activity_settings);
             setupUI();
@@ -65,6 +78,8 @@ public class SettingsActivity extends AppCompatActivity {
             setupUI_2();
         }
     }
+
+
 
     @Override
     protected void onStop() {
@@ -86,12 +101,77 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
         logOutBtn = (Button) findViewById(R.id.button);
+
+        if(personal_info==null) {
+            String dataURL = "http://polaris.i3l.gatech.edu:8080/gt-fhir-webapp/base/Patient/" + ID + "?_format=json";
+            new AsyncTask<String, Void, String>() {
+                @Override
+                protected String doInBackground(String... dataurls) {
+                    try {
+                        return sendGet(dataurls[0]);
+                    } catch (Exception e) {
+                        return "error " + e.getMessage();
+                    }
+                }
+
+                @Override
+                protected void onPostExecute(String result) {
+                    JSONObject myJO;
+                    try {
+                        myJO = new JSONObject(result);
+                        personal_info = parseJsonObject(myJO);
+                        healthInfo.setText(personal_info);
+                    } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }.execute(dataURL);
+        }
+        else
+            healthInfo.setText(personal_info);
+
+
+
+
+
+
+
+
+
+
+
     }
 
     private void setupUI() {
         // Toolbar
         editText_0 = (EditText) findViewById(R.id.usernameText);
         editText_1 = (EditText) findViewById(R.id.passwordText);
+
+
+
+        editText_0.setOnFocusChangeListener(
+                new View.OnFocusChangeListener() {
+                    @Override
+                    public void onFocusChange(View v, boolean hasFocus) {
+                        if(hasFocus)
+                                editText_0.setText("");
+                    }
+                }
+        );
+
+        editText_1.setOnFocusChangeListener(
+                new View.OnFocusChangeListener() {
+                    @Override
+                    public void onFocusChange(View v, boolean hasFocus) {
+                        if(hasFocus)
+                                editText_1.setText("");
+                    }
+                }
+        );
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Settings");
         setSupportActionBar(toolbar);
@@ -107,6 +187,7 @@ public class SettingsActivity extends AppCompatActivity {
 
     public void myLogout(View v){
         ID = null;
+        personal_info=null;
         Toast.makeText(getApplicationContext(),"Successfully log out.", Toast.LENGTH_SHORT).show();
         finish();
         //这里log out之后结束，就要求在别的fragment中加入re-layout的功能，比如 onResume（）
@@ -117,6 +198,68 @@ public class SettingsActivity extends AppCompatActivity {
     public void myLogin(View v){
         myFirebaseRef.child(editText_0.getText().toString()).addValueEventListener(myVEListenner);
     }
+
+
+
+
+    private String sendGet(String url) throws Exception {
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+        return response.toString();
+
+    }
+
+
+    private String parseJsonObject(JSONObject myJo)
+    {
+        String info="";
+        try {
+            JSONArray name=myJo.getJSONArray("name");
+            String family=name.getJSONObject(0).getJSONArray("family").getString(0);
+            String given=name.getJSONObject(0).getJSONArray("given").getString(0);
+            String gender=myJo.getString("gender");
+            String birthDate=myJo.getString("birthDate");
+            info+="Name: "+given+" "+family+"\n\n";
+            info+="Gender: "+gender+"\n\n";
+            info+="Birth date: "+birthDate+"\n";
+            return info;
+        }
+        catch(Exception e)
+        {
+
+        }
+        return "";
+    }
+
+
+
+
+
+
+
+
+
 
 
 
